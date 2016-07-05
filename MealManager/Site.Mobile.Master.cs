3295b93d8ee8876337b7f34438e3b32e2efd3cfa
Steve Myers <svenoaks@gmail.com>
2016-07-05 13:09:39 -0400

    disable mobile master

--- MealManager/Site.Mobile.Master.cs
+++ MealManager/Site.Mobile.Master.cs
@@ -11,7 +11,11 @@ namespace MealManager
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            var AlternateView = "Desktop";
+            var switchViewRouteName = "AspNet.FriendlyUrls.SwitchView";
+            var url = GetRouteUrl(switchViewRouteName, new { view = AlternateView, __FriendlyUrls_SwitchViews = true });
+            url += "?ReturnUrl=" + HttpUtility.UrlEncode(Request.RawUrl);
+            Response.Redirect(url);
         }
     }
 }
